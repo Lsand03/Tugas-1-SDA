d40e0575204af191fc356d87a2e4f63139bd1152
Muhammad Afif Elsanda <95417491+Lsand03@users.noreply.github.com>
2022-02-26 21:59:23 +0700

    Update soal4.java

--- src/TugasSDA1/soal4.java
+++ src/TugasSDA1/soal4.java
@@ -11,14 +11,12 @@ public class soal4 {
     nama.add("a");
     
     //Soal no 4
-    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-    //indexOf(a), indexOf(c), indexOf(q)
+    //Tentukan hasil yang didapatkan ketika dilakukan perintah indexOf(a), indexOf(c), indexOf(q)
 
     System.out.println(nama.indexOf("a"));
     System.out.println(nama.indexOf("c"));
     System.out.println(nama.indexOf("q"));
-    }
-    
+    }   
 }
     
 
