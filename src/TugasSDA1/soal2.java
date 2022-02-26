2f044541b3a3b2dfd95588932cd3f277bc42e0d2
Muhammad Afif Elsanda <95417491+Lsand03@users.noreply.github.com>
2022-02-26 21:35:33 +0700

    Add files via upload

---
+++ src/TugasSDA1/soal2.java
@@ -0,0 +1,12 @@
+package TugasSDA1;
+
+public class soal2 {
+    public static void main(String[] args) {
+        
+    String[] nama = {"N","D","R","A"};
+    //Soal no 2
+    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+    //size()
+    System.out.println("ukuran array adalah " + nama.length );
+    }    
+}
---
+++ src/TugasSDA1/soal4.java
@@ -0,0 +1,24 @@
+package TugasSDA1;
+import java.util.ArrayList;
+
+public class soal4 {
+    public static void main(String[] args) {
+    
+    ArrayList<String> nama = new ArrayList<String>();
+    nama.add("a");
+    nama.add("n");
+    nama.add("d");
+    nama.add("a");
+    
+    //Soal no 4
+    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+    //indexOf(a), indexOf(c), indexOf(q)
+
+    System.out.println(nama.indexOf("a"));
+    System.out.println(nama.indexOf("c"));
+    System.out.println(nama.indexOf("q"));
+    }
+    
+}
+    
+
