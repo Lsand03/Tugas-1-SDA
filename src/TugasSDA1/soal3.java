46ee7f3b68577672dc8d42032323b3d69f4ca451
Muhammad Afif Elsanda <95417491+Lsand03@users.noreply.github.com>
2022-02-26 21:58:37 +0700

    Update soal3.java

--- src/TugasSDA1/soal3.java
+++ src/TugasSDA1/soal3.java
@@ -10,9 +10,10 @@ public class soal3 {
     nama.add("N");
     nama.add("D");
     nama.add("A");
+    
     //Soal no 3
-    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-    //get(0), get(2), get(6), get(-3)
+    //Tentukan hasil yang didapatkan ketika dilakukan perintah get(0), get(2), get(6), get(-3)
+
     System.out.println(nama.get(0));
     System.out.println(nama.get(2));
     System.out.println(nama.get(6));
