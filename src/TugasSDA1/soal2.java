de90b5d4f40a2ffb2d385d78c8a103f2ca07ed7a
Muhammad Afif Elsanda <95417491+Lsand03@users.noreply.github.com>
2022-02-26 21:50:47 +0700

    Update soal2.java

--- src/TugasSDA1/soal2.java
+++ src/TugasSDA1/soal2.java
@@ -3,7 +3,7 @@ package TugasSDA1;
 public class soal2 {
     public static void main(String[] args) {
         
-    String[] nama = {"N","D","R","A"};
+    String[] nama = {"A","N","D","A"};
     //Soal no 2
     //Tentukan hasil yang didapatkan ketika dilakukan perintah size()
     System.out.println("ukuran array adalah " + nama.length );
